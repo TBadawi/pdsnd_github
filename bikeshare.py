de4b2c2dcc391a95f358453cdd3290d69efde8f4
Tarek Elbadawi <elbadawi.t@gmail.com>
2021-02-23 13:54:10 +0300

    Removed print (df) under def load_data. Data will be displayed later under user request.

--- bikeshare.py
+++ bikeshare.py
@@ -70,7 +70,6 @@ def load_data(city, month, day):
     if day != 'all':
         # filter by day of week to create the new dataframe
         df = df[df['day_of_week'] == day.title()]
-    print (df)
     print (df.columns)
 
     return df
